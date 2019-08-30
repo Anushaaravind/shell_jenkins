fedb999d10fbfbbc34062b56b5637165aa8b3a14
root <root@localhost.localdomain>
2019-08-30 18:35:31 +0000

    added

---
+++ equal.sh
@@ -0,0 +1,7 @@
+read -p "Enter first string: " VAR1
+ read -p "Enter second string: " VAR2  
+if [[ "$VAR1" == "$VAR2" ]]; then 
+    echo "Strings are equal."
+ else 
+    echo "Strings are not equal." 
+fi
